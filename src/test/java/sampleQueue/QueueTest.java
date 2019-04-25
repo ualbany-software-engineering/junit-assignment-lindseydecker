b40e023be0fc591543145782eb05937f65def542
Ninad P.Chaudhari <ninadpchaudhari@gmail.com>
2019-04-25 01:42:02 -0400

    Initial Commit with Queue and QueueTest

---
+++ src/test/java/sampleQueue/QueueTest.java
@@ -0,0 +1,53 @@
+package sampleQueue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+
+class QueueTest {
+
+	/**
+	 * Tests for Queue.
+	 */
+
+	private static final String SOME_ITEM = "some-content";
+	private Queue<String> q;
+
+
+	@Test
+	@Disabled
+	@DisplayName("is instantiated with new Queue()")
+	void isInstantiatedWithNew() {
+		new Queue<>();
+	}
+
+	@BeforeEach
+	void init() {
+		this.q = new Queue<String>();
+	}
+
+	@Test
+	@DisplayName("Verify Queue isEmpty when queue is initialized")
+	void isEmptyShouldGiveTrueOnQueueInit() {
+		assertTrue(q.isEmpty());
+	}
+
+	//Example of Wrong Test! 
+	@Test
+	@Disabled
+	@DisplayName("Verify Queue isEmpty returns false when queue is not empty")
+	void isEmptyShouldGiveFalseWhenQueueIsNotEmpty() {
+		this.q.enqueue(SOME_ITEM);
+		assertFalse(q.isEmpty());
+	}
+
+	
+}
